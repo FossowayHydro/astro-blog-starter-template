d344fdd8fa61e0c21fc51d14e7fa6dc8dda3c3d6
FossowayHydro <fossowayhydro@gmail.com>
2026-02-03 15:49:33 +0000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Fossoway Hydro Fund";
+export const SITE_DESCRIPTION = "A Small Grants scheme operated by Fossoway & District Community Council, applying funds provided through the Rumbling Bridge Community Hydro Society";
